1cfcdb6b530e3384b2d4e4f616d45fbafba0446f
aymankabakibi <aymankabakibi@cpe-70-95-66-162.san.res.rr.com>
2021-05-05 18:38:06 -0700

    I Finished Wrong Way Cow, the commit wasn't working, start on number 7 of Intro To Array Lists

--- src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
+++ src/_00_Intro_To_ArrayLists/_01_IntroToArrayLists.java
@@ -1,21 +1,41 @@
 package _00_Intro_To_ArrayLists;
 
+import java.util.ArrayList;
+
 public class _01_IntroToArrayLists {
     public static void main(String[] args) {
         // 1. Create an array list of Strings
         //    Don't forget to import the ArrayList class
-
+    	ArrayList<String> strings=new ArrayList<String>();
         // 2. Add five Strings to your list
-
+    	strings.add("Bob");
+    	strings.add("Liverpool");
+    	strings.add("Gday");
+    	strings.add("Hello");
+    	strings.add("Wassup");
         // 3. Print all the Strings using a standard for-loop
-
+    	for(int i=0; i<strings.size(); i++) {
+    		System.out.println(""+strings.get(i));
+    	}
         // 4. Print all the Strings using a for-each loop
-
+    	for(String s: strings) {
+    		System.out.println(""+s);
+    	}
         // 5. Print only the even numbered elements in the list.
-
+    		for(int i=0; i<strings.size(); i++) {
+    			if(i%2==0) {
+    				System.out.println(""+strings.get(i));
+    			}
+    		}
         // 6. Print all the Strings in reverse order.
-
+    		for(int i=strings.size()-1;i>=0; i--) {
+    			System.out.println(""+strings.get(i));
+    		}
         // 7. Print only the Strings that have the letter 'e' in them.
-        
+    		for(int i=0; i<strings.size(); i++) {
+    			if(strings.get(i).
+    					//IDK HOW TO DO THIS, I NEED TO CHECK IF EACH STRING IN THE ARRAYLIST HAS AN E IN IT, I TRIED .CONTAINS AND OTHER METHODS BUT IT DIDN'T WORK
+    					//ASK TEACHER HOW TO CHECK IF A STRING HAS A LETTER IN IT
+    		}
     }
 }
